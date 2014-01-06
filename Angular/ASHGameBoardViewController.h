8536cacf39a8c350703b6ee731f5f7e2e413d700
Ash Furrow <ash@ashfurrow.com>
2014-01-05 19:31:40 -0500

    Initial commit.

---
+++ Angular/ASHGameBoardViewController.h
@@ -0,0 +1,13 @@
+//
+//  ASHGameBoardViewController.h
+//  Angular
+//
+//  Created by Ash Furrow on 1/5/2014.
+//  Copyright (c) 2014 Ash Furrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASHGameBoardViewController : UIViewController
+
+@end
---
+++ Angular/ASHGameBoardViewController.m
@@ -0,0 +1,38 @@
+//
+//  ASHGameBoardViewController.m
+//  Angular
+//
+//  Created by Ash Furrow on 1/5/2014.
+//  Copyright (c) 2014 Ash Furrow. All rights reserved.
+//
+
+#import "ASHGameBoardViewController.h"
+
+@interface ASHGameBoardViewController ()
+
+@end
+
+@implementation ASHGameBoardViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
