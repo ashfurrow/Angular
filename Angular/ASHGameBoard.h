870fde280904385d3a037fc131b7488c1d84b146
Ash Furrow <ash@ashfurrow.com>
2014-01-05 20:01:35 -0500

    Added ASHGameBoard.

--- Angular/ASHGameBoard.h
+++ Angular/ASHGameBoard.h
@@ -8,6 +8,25 @@
 
 #import <Foundation/Foundation.h>
 
+struct ASHGameBoardPoint {
+    NSUInteger x;
+    NSUInteger y;
+};
+
+typedef struct ASHGameBoardPoint ASHGameBoardPoint;
+
+
+typedef NS_ENUM(NSUInteger, ASHGameBoardPositionState) {
+    ASHGameBoardPositionStateUndecided = 0,
+    ASHGameBoardPositionStatePlayerA,
+    ASHGameBoardPositionStatePlayerB
+};
+
 @interface ASHGameBoard : NSObject
+
+@property (nonatomic, readonly) NSUInteger width;
+@property (nonatomic, readonly) NSUInteger height;
+
+-(ASHGameBoardPositionState)stateForPosition:(ASHGameBoardPoint)point;
 
 @end
--- Angular/ASHGameBoard.m
+++ Angular/ASHGameBoard.m
@@ -8,6 +8,49 @@
 
 #import "ASHGameBoard.h"
 
+@interface ASHGameBoard ()
+
+@property (nonatomic, assign) NSUInteger width;
+@property (nonatomic, assign) NSUInteger height;
+
+@property (nonatomic, assign) ASHGameBoardPositionState *board;
+
+@end
+
 @implementation ASHGameBoard
+
+-(instancetype)initWithWidth:(NSUInteger)width height:(NSUInteger)height {
+    self = [super init];
+    if (self == nil) return nil;
+    
+    self.width = width;
+    self.height = height;
+    
+    self.board = malloc(sizeof(ASHGameBoardPositionState) * self.width * self.height);
+    
+    return self;
+}
+
+-(void)dealloc {
+    free(self.board);
+}
+
+#pragma mark - Private Methods
+
+-(ASHGameBoardPositionState*)positionStateAtPoint:(ASHGameBoardPoint)point {
+    return &self.board[point.x * self.width + point.y];
+}
+
+#pragma mark - Public Methods
+
+-(ASHGameBoardPositionState)stateForPosition:(ASHGameBoardPoint)point {
+    ASHGameBoardPositionState *pointer = [self positionStateAtPoint:point];
+    return *pointer;
+}
+
+-(void)setState:(ASHGameBoardPositionState)state forPosition:(ASHGameBoardPoint)point {
+    ASHGameBoardPositionState *pointer = [self positionStateAtPoint:point];
+    *pointer = state;
+}
 
 @end
