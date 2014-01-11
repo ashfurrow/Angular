a816294418e7046f64e2c83f3a22775cf8bc55d5
Ash Furrow <ash@ashfurrow.com>
2014-01-11 17:36:32 -0500

    Integrated crashlytics.

--- Angular/ASHAppDelegate.m
+++ Angular/ASHAppDelegate.m
@@ -8,12 +8,15 @@
 
 #import "ASHAppDelegate.h"
 
+#import <Crashlytics/Crashlytics.h>
+
 @implementation ASHAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
     
+    [Crashlytics startWithAPIKey:@"e92912505e9b6327bad28022d04290c3ddffaa82"];
     self.window.tintColor = [UIColor colorWithHexString:@"FF0062"];
     
     return YES;
